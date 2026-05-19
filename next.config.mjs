3cab2a0c03e84c6724bf8a543a131a2172918fac
sergio <boza.marin@gmail.com>
2026-05-19 11:54:57 -0400

    fix: configure module alias resolution for render build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      { source: "/oauth2/authorize", destination: "/oauth2/v1/authorize" },
-      { source: "/oauth2/par", destination: "/oauth2/v1/authorize/par" },
-      { source: "/oauth2/token", destination: "/oauth2/v1/token" },
-      { source: "/oauth2/revoke", destination: "/oauth2/v1/revoke" },
-      { source: "/oauth2/jwks", destination: "/oauth2/v1/certs" }
-    ];
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
